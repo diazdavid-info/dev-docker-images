3538ba4277e4aad2553b25811f39b3b658c953be
David Diaz <diazdavid.info@gmail.com>
2020-09-12 19:11:31 +0200

    delete directory httpd

--- httpd/2.4.43/docker-generate-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-while read name; do
-    echo "$name"
-done < <(curl --unix-socket /var/run/docker.sock http://localhost/containers/a7af1231fca1084180e8a64b3e1ed5e9ae91bff886e253c69fabc4e9d1b4a90a/json | jq -r '.Config.Env')
-
