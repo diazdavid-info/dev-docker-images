81ce141e0be95b9dbaa757fe2c380b7db7016867
David Diaz <diazdavid.info@gmail.com>
2024-07-04 08:34:52 +0200

    add high load express server

---
+++ high-load-express/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+
+const port = 3000
+const app = express()
+
+let largeArray = []
+
+// Ruta que consume mucha CPU
+app.get('/cpu', (req, res) => {
+  const start = Date.now()
+  while (Date.now() - start < 10000) {
+    // Hacer cálculos intensivos durante 10 segundos
+    Math.sqrt(Math.random())
+  }
+  res.send('CPU intensive task completed')
+})
+
+// Ruta que consume mucha memoria
+app.get('/memory', (req, res) => {
+  let list = []
+  for (let i = 0; i < 1e7; i++) {
+    list.push(Math.random())
+  }
+  largeArray.push([...list])
+  list = []
+  res.send('Memory intensive task completed')
+})
+
+// Ruta que consume mucha memoria
+app.get('/clear-memory', (req, res) => {
+  largeArray = []
+  global.gc()
+  res.send('Clear memory task completed')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
